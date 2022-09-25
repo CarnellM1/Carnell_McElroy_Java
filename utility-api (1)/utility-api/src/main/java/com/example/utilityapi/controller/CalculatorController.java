98be9528a70967ddea3a3d5870763f76aada610c
carne <c.d.mcelroy@spartans.nsu.edu>
2022-09-25 16:02:02 -0400

    Module 5 challenge

---
+++ utility-api (1)/utility-api/src/main/java/com/example/utilityapi/controller/CalculatorController.java
@@ -0,0 +1,28 @@
+package com.example.utilityapi.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class CalculatorController {
+
+    public CalculatorController() {
+    }
+
+    @RequestMapping(value = "/calculator/divide", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public double divide(@RequestParam int value1, @RequestParam int value2) {
+        if(value2 == 0)
+        {
+            throw new IllegalArgumentException("will result as undefined");
+        }
+        return value1 / value2;
+    }
+
+    @RequestMapping(value = "/calculator/square/{value}", method = RequestMethod.GET)
+    @ResponseStatus(value = HttpStatus.OK)
+    public int square(@PathVariable int value) {
+
+        return value * value;
+    }
+}
